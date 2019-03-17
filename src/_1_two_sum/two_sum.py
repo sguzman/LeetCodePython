50403bef2f3c7a4dc4ce8cdc9309c683eac7773c
Salvador Guzman <guzmansalv@gmail.com>
2019-03-16 17:22:36 -0700

    first draft

--- src/_1_two_sum/two_sum.py
+++ src/_1_two_sum/two_sum.py
@@ -1,7 +1,21 @@
 from typing import List
+from typing import Dict
 
 
 class Solution:
     @staticmethod
     def twoSum(nums: List[int], target: int) -> List[int]:
-        return [0, 1]
+        storage: Dict[int, int] = {}
+        answer: List[int] = [0, 1]
+
+        for i, n in enumerate(nums):
+            diff = target - n
+            idx = storage.get(diff)
+
+            if idx is None:
+                storage[diff] = i
+            else:
+                answer = [idx, i]
+                break
+
+        return answer
