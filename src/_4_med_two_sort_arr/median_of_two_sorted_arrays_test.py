b84e40ad7ee87fb3a608f28dc0c09701f1742db7
Salvador Guzman <guzmansalv@gmail.com>
2019-03-19 00:40:07 -0700

    stub and tests

---
+++ src/_4_med_two_sort_arr/median_of_two_sorted_arrays_test.py
@@ -0,0 +1,17 @@
+import unittest
+from _4_med_two_sort_arr import median_of_two_sorted_arrays
+
+
+class MyTestCase(unittest.TestCase):
+    def test_1(self):
+        solution = median_of_two_sorted_arrays.Solution()
+        nums1 = [1, 3]
+        nums2 = [2]
+
+        output = 2
+        actual = solution.findMedianSortedArrays(nums1, nums2)
+        self.assertEqual(output, actual)
+
+
+if __name__ == '__main__':
+    unittest.main()
