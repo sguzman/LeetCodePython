b9fe988044dc3c0be4a0812292d33c86250b44c7
Salvador Guzman <guzmansalv@gmail.com>
2019-03-19 00:15:33 -0700

    first draft

--- src/_3_long_rep_wo_rep/longest_substring_without_repeating_characters.py
+++ src/_3_long_rep_wo_rep/longest_substring_without_repeating_characters.py
@@ -1,3 +1,18 @@
+from typing import Dict
+
+
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        return len(s)
+        last_seen: Dict[str, int] = {}
+        global_sum: int = 0
+        start_idx: int = 0
+
+        for i, c in enumerate(s):
+            if last_seen.get(c) is None:
+                global_sum = max(i - start_idx + 1, global_sum)
+            elif start_idx <= last_seen[c]:
+                start_idx += last_seen[c] + 1
+
+            last_seen[c] = i
+
+        return global_sum
