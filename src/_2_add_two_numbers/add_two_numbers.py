5b8a2320007881239eef3f5fd2bb00e619ca2b45
Salvador Guzman <guzmansalv@gmail.com>
2019-03-18 22:48:51 -0700

    first try

--- src/_2_add_two_numbers/add_two_numbers.py
+++ src/_2_add_two_numbers/add_two_numbers.py
@@ -14,4 +14,25 @@ class ListNode:
 class Solution:
     @staticmethod
     def addTwoNumbers(l1: ListNode, l2: ListNode) -> ListNode:
-        return l1
+        carry = 0
+
+        base = ListNode(0)
+        base_idx = base
+
+        l1_idx = l1
+        l2_idx = l2
+
+        while l1_idx is not None and l2_idx is not None:
+            local_sum = l1.val + l2.val + carry
+            if local_sum > 9:
+                carry = 1
+                local_sum = local_sum - 10
+            else:
+                carry = 0
+            base_idx.val = local_sum
+            base_idx.next = ListNode(0)
+
+            l1_idx = l1_idx.next
+            l2_idx = l2_idx.next
+
+        return base
