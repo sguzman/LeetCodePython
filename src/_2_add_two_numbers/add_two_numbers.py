67edc509b675df08db6e87444f4ad4928a67eeb5
Salvador Guzman <guzmansalv@gmail.com>
2019-03-18 22:42:11 -0700

    solution stub and test case

---
+++ src/_2_add_two_numbers/add_two_numbers.py
@@ -0,0 +1,17 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution:
+    @staticmethod
+    def addTwoNumbers(l1: ListNode, l2: ListNode) -> ListNode:
+        return l1
