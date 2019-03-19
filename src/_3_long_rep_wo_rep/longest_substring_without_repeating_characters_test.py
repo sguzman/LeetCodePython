2d63c2307f53d26007e401b9623ac30741af9118
Salvador Guzman <guzmansalv@gmail.com>
2019-03-18 23:13:48 -0700

    solution stub and test

---
+++ src/_3_long_rep_wo_rep/longest_substring_without_repeating_characters_test.py
@@ -0,0 +1,35 @@
+import unittest
+from _3_long_rep_wo_rep import longest_substring_without_repeating_characters
+
+
+class MyTestCase(unittest.TestCase):
+    def test_1(self):
+        solution = longest_substring_without_repeating_characters.Solution()
+
+        input_1: str = "abcabcbb"
+        output: int = 3
+        actual: int = solution.lengthOfLongestSubstring(input_1)
+
+        self.assertEqual(output, actual)
+
+    def test_2(self):
+        solution = longest_substring_without_repeating_characters.Solution()
+
+        input_1: str = "bbbbb"
+        output: int = 1
+        actual: int = solution.lengthOfLongestSubstring(input_1)
+
+        self.assertEqual(output, actual)
+
+    def test_3(self):
+        solution = longest_substring_without_repeating_characters.Solution()
+
+        input_1: str = "pwwkew"
+        output: int = 3
+        actual: int = solution.lengthOfLongestSubstring(input_1)
+
+        self.assertEqual(output, actual)
+
+
+if __name__ == '__main__':
+    unittest.main()
