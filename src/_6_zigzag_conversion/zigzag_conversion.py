d1232fbd56cae48391d80451cd90540ad269b2e7
Salvador Guzman <guzmansalv@gmail.com>
2019-04-14 15:29:10 -0700

    first draft

--- src/_6_zigzag_conversion/zigzag_conversion.py
+++ src/_6_zigzag_conversion/zigzag_conversion.py
@@ -1,3 +1,41 @@
+from typing import List
+
+
 class Solution:
-    def convert(self, s: str, num_rows: int) -> str:
-        return s[:num_rows]
+    def convert(self, s: str, n: int) -> str:
+        store: List[str] = []
+        if n is 1:
+            return s
+
+        if len(s) <= n:
+            return s
+
+        # First row
+        i: int = 0
+        while i < len(s):
+            store.append(s[i])
+            i += (2 * i) - 1
+
+        # Middle rows
+        for j in range(1, n):
+            k: int = j
+            column: bool = True
+            column_to_diag_offset: int = 2 * (n - j - 1)
+            diag_to_column_offset: int = 2 * j
+
+            while k < len(s):
+                store.append(s[k])
+                if column:
+                    k += column_to_diag_offset
+                    column = False
+                else:
+                    k += diag_to_column_offset
+                    column = True
+
+        # Last row
+        i = n - 1
+        while i < len(s):
+            store.append(s[i])
+            i += (2 * i) - 1
+
+        return ''.join(store)
